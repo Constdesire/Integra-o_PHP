96233c64c87ca8650491307c32410aa44bdc1f16
Desirée Constantino <117848187+Constdesire@users.noreply.github.com>
2024-03-13 14:39:32 -0300

    Add files via upload

---
+++ views/listar.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listar</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  
+</head>
+
+    <body>
+<div class="container">
+  <div class="row">
+    <div class="col">
+          <nav class="navbar navbar-expand-lg bg-body-tertiary">
+              <div class="container-fluid text-bg-primary p-3">
+                  <a class="navbar-brand text-light" href="#" >SISTEMA WEB</a>
+                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                       <span class="navbar-toggler-icon"></span>
+                     </button>
+                          <div class="collapse navbar-collapse" id="navbarNav">
+                                <ul class="navbar-nav">
+                                 <li class="nav-item">
+                                  <a class="nav-link active text-light" aria-current="page" href="#">Cadastrar</a>
+                                 </li>
+                                    <li class="nav-item">
+                                       <a class="nav-link text-light" href="#">Consultar</a>
+                                    </li>
+                               </ul>
+                         </div>
+                  </div>
+            </div>
+           <p> 
+           <p align="left"><font size="5">Cadastrar -  Agendamento de Potenciais Clientes</p></font>
+           <p align="left"><font size="4">Sistema utilizado para agedamento de serviços</font></p>
+           <p>
+           <div class="col">
+           <div class="overflow-auto">
+           <table class="table">
+               <thead class="table-primary">
+               <tr>
+                      <th scope="col">Nome</th>
+                      <th scope="col">Telefone</th>
+                      <th scope="col">Origem</th>
+                      <th scope="col">Data do contato</th>
+                      <th scope="col">Observações</th>
+                   </tr>
+               </thead>
+               <tbody>
+                   <tr>
+                      <th scope="col">Alice</th>
+                      <th scope="col">(11)33251=3524</th>
+                      <th scope="col">Celular</th>
+                      <th scope="col">11/11/2011</th>
+                      <th scope="col">Nenhuma</th>
+                   </tr>
+                   <tr>
+                      <th scope="col">Mario</th>
+                      <th scope="col">(11)27489=3288</th>
+                      <th scope="col">Celular</th>
+                      <th scope="col">12/12/2011</th>
+                      <th scope="col">Nenhuma</th>
+                   </tr>
+                   <tr>
+                      <th scope="col">Carlos</th>
+                      <th scope="col">(11)64338=8438</th>
+                      <th scope="col">Celular</th>
+                      <th scope="col">13/01/2012</th>
+                      <th scope="col">Nenhuma</th>
+                   </tr>
+               </tbody>
+             </table>
+             </div>
+            </div>
+         </div>
+     </div>
+    </body>
+</html>
